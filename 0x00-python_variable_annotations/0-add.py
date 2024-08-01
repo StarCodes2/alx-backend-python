6f6cede483f6ecdf59c78b4f8680bd86c843f551
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-01 10:54:49 +0000

    To add two variables of type float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Defines a type-annotated function add that takes a float 'a' and
+    a float 'b' as arguments and returns their sum as a float.
+"""
+
+def add(a: float, b: float) -> float:
+    """ Returns the addition of two float typed variables. """
+    return a + b
--- 0x00-python_variable_annotations/README.md
+++ 0x00-python_variable_annotations/README.md
@@ -1,2 +1,3 @@
 # 0x00. Python - Variable Annotations
 ## Files:
+- **0-add.py**: Hold a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
