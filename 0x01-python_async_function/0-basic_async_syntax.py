5dd81352566b8ecba077a91a1b16ba4b478fc9a6
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-05 20:50:06 +0000

    To delay an asynchronous coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Defines an asynchronous coroutine that takes in an int argument (max_delay)
+    and waits for a random delay between 0 and max_delay
+"""
+import asyncio
+import random
+import time
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Waits for a specicied amount of seconds. """
+    if max_delay < 1:
+        delay: int = 0
+    else:
+        delay: int = random.randint(0, max_delay)
+    now: float = time.perf_counter()
+    await asyncio.sleep(delay)
+
+    return (time.perf_counter() - now)
