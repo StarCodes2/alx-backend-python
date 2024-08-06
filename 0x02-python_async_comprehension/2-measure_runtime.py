b37c3480bb4d3eb69c387157dbb1f641242b1f38
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-06 19:37:01 +0000

    BUG: used pref_counter instead of perf_counter.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,7 +11,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """ Measures the time it takes to execute async_comprehension 4 times """
-    start = time.pref_counter()
+    start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
 
     return time.perf_counter() - start
