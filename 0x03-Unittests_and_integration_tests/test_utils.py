a09ae7d38cf91942f128f8841c3b459f5df90acd
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-25 20:25:35 +0000

    To parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,11 +11,11 @@ from typing import Any, Mapping, Sequence
 class TestAccessNestedMap(unittest.TestCase):
     """ Define test methods access_nested_map(). """
     @parameterized.expand([
-        ("Single item in map and path", {"a": 1}, ("a",), 1),
-        ("Nested map/single key", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("Nexted map/two keys", {"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, name: str, nested_map: Mapping[str, Any],
+    def test_access_nested_map(self, nested_map: Mapping[str, Any],
                                path: Sequence[str], result: Any) -> None:
         """ Tests the access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), result)
