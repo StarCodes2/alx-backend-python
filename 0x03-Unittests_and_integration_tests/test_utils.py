035d2281eeae3b4f9c676c03e81f030270b66a67
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-26 16:28:57 +0000

    To test if a decorator use for memoization works

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 """
 import unittest
 from unittest.mock import Mock, patch
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from typing import Any, Dict, Mapping, Sequence
 
@@ -52,3 +52,31 @@ class TestGetJson(unittest.TestCase):
         result = get_json(url)
         mock_get.assert_called_once_with(url)
         self.assertEqual(result, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Defines a test the memoize decorator in utils module. """
+    def test_memoize(self) -> None:
+        """ Test that the memoize decorator caches the result. """
+        class TestClass:
+            """ Defines a method that uses the memoize decorator. """
+            def a_method(self) -> int:
+                """ Returns 42. """
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                """ Returns the return value of a_method. """
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method',
+                          return_value=42) as mock_method:
+            test_result1 = test_obj.a_property
+            test_result2 = test_obj.a_property
+
+            self.assertEqual(test_result1, 42)
+            self.assertEqual(test_result2, 42)
+
+            mock_method.assert_called_once()
